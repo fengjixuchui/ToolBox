869e056c3915db26fee8d07dfe079a900798afe2
ESTBLC <estblcsk@gmail.com>
2018-12-28 21:34:45 +0100

    Test linking

--- list/test.c
+++ list/test.c
@@ -23,14 +23,26 @@ Test(list, push) {
 
     list_push(list, &e);
     struct node *node = list->first;
+    /* Test if the first node is pointed by first and last */
     cr_assert(list->last == node);
+    /* Test extremity form node */
+    cr_assert(!node->next && !node->prev);
+    /* Check stored value */
     cr_assert(*(int *)node->data == e);
+    /* Check list size */
     cr_assert(list->size == 1);
 
     list_push(list, &e2);
-    node = list->first;
-    cr_assert(list->last != node);
-    cr_assert(*(int *)node->data == e2);
+    struct node *node2 = list->first;
+    /* Test if the first node is pointed only by first */
+    cr_assert(list->last != node2);
+    /* Test extremity form node */
+    cr_assert(!node2->prev);
+    /* Test interconnexion between nodes */
+    cr_assert(node->prev == node2 && node2->next == node);
+    /* Check stored value */
+    cr_assert(*(int *)node2->data == e2);
+    /* Check list size */
     cr_assert(list->size == 2);
 
     list_free(list);
@@ -69,14 +81,26 @@ Test(list, append) {
 
     list_append(list, &e);
     struct node *node = list->last;
+    /* Test if the first node is pointed by first and last */
     cr_assert(list->first == node);
+    /* Test extremity form node */
+    cr_assert(!node->next && !node->prev);
+    /* Check stored value */
     cr_assert(*(int *)node->data == e);
+    /* Check list size */
     cr_assert(list->size == 1);
 
     list_append(list, &e2);
-    node = list->last;
-    cr_assert(list->first != node);
-    cr_assert(*(int *)node->data == e2);
+    struct node *node2 = list->last;
+    /* Test if the first node is only pointed by last */
+    cr_assert(list->first != node2);
+    /* Test extremity form node */
+    cr_assert(!node2->next);
+    /* Test interconnexion between nodes */
+    cr_assert(node2->prev == node && node->next == node2);
+    /* Check stored value */
+    cr_assert(*(int *)node2->data == e2);
+    /* Check list size */
     cr_assert(list->size == 2);
 
     list_free(list);
