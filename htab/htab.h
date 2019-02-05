2dceafa6b106396df6cb3ee679db70fc58572d02
ESTBLC <estblcsk@gmail.com>
2019-02-05 18:53:06 +0000

    Add htab and its declaration

---
+++ htab/htab.h
@@ -0,0 +1,28 @@
+#ifndef HTAB_H
+#define HTAB_H
+
+#include <stddef.h>
+
+/* An htab need an hash function given by the user */
+struct htab {
+    size_t cap;
+    size_t size;
+    size_t (*hfunc)(void *);
+    void *data;
+};
+
+typedef struct htab htab_t;
+
+/* Create a new htab */
+htab_t *htab_new();
+/* Delete the htab */
+void htab_free(htab_t *htab, size_t (*hfunc)(void *));
+
+/* Add a new element in the htab */
+void htab_add(htab_t *htab, void *data);
+/* Remove an element of the htab */
+void *htab_del(htab_t *htab);
+/* Find a match in the htab */
+void *htab_find(htab_t *htab, void *data);
+
+#endif
